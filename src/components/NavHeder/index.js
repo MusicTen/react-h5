9ccd0657cb74d42fc23ea14684917fd4c60a794b
MusicTen <1765408313@qq.com>
2020-08-27 16:04:52 +0800

    feat: 🎸 引入withRouter

--- src/components/NavHeder/index.js
+++ src/components/NavHeder/index.js
@@ -1,17 +1,23 @@
 import React from 'react'
-
+import { withRouter } from 'react-router'
 import { NavBar, Icon } from 'antd-mobile'
-
 import './index.scss'
 
-export default function NavHeader(props) {
+function NavHeader(props) {
+  function goback() {
+    console.log(props.history)
+    props.history.goBack()
+  }
   return (
     <NavBar
       mode="light"
-      icon={<Icon type="left" />}
+      icon={<Icon onClick={() => goback()} type="left" />}
       onLeftClick={() => console.log('onLeftClick')}
     >
       { props.title }
     </NavBar>
   )
 }
+
+// withRouter: 把不是通过路由切换过来的组件中，将react-router 的 history、location、match 三个对象传入props对象上
+export default withRouter(NavHeader)
